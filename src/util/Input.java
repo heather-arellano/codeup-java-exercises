a9626fa5866e8d3ed4abea63b8c68fb4262047e7
heather arellano <hma.485@icloud.com>
2018-05-15 10:16:08 -0500

    Commit

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,16 +3,52 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private Scanner scanner;
+    private Scanner scan;
 
-    public Input(Scanner scanner) {
-        try (Scanner scanner1 = this.scanner = scanner) {
+    public Input() {
+       this.scan = new Scanner(System.in).useDelimiter("\n");
         }
     }
     public String getString(){
+        return this.scan.next();
+        }
 
-
+    public String getString(String  prompt) {
+        System.out.println(prompt);
+        return this.scan.next();
     }
+
+
+    public boolean yesNo() {
+    String input = this.scan.next();
+    if(input.equalsIgnoreCase("y" || input.equalsIgnoreCase("yes")) {
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        String input = this.scan.next();
+        if(input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
+            return true;
+        } else {
+            return false;
+        }
+
+    public int getInt(int min, int max, String prompt) {
+            System.out.println(prompt);
+
+            int userInput = this.getInt();
+
+            if(userInout > max || userInput < min) {
+                return getInt(min, max);
+            } else {
+                return userInput;
+            }
+        }
 
 
 
